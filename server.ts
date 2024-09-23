d4aad6477d0ef4adea2ac05412fd99c70624b6af
JeanCarlos Jimenez <jeancarlosjimenezc25@gmail.com>
2024-09-23 09:05:32 -0500

    Changes Files - Angular

--- server.ts
+++ server.ts
@@ -5,26 +5,28 @@ import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
 
-//* The Express app is exported so that it can be used by serverless Functions.
+// The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
   const serverDistFolder = dirname(fileURLToPath(import.meta.url));
   const browserDistFolder = resolve(serverDistFolder, '../browser');
   const indexHtml = join(serverDistFolder, 'index.server.html');
+
   const commonEngine = new CommonEngine();
 
   server.set('view engine', 'html');
   server.set('views', browserDistFolder);
 
-  //* Example Express Rest API endpoints
+  // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
-  server.get('*.*', express.static(browserDistFolder, {
-    maxAge: '1y'
+  server.get('**', express.static(browserDistFolder, {
+    maxAge: '1y',
+    index: 'index.html',
   }));
 
-  //* All regular routes use the Angular engine
-  server.get('*', (req, res, next) => {
+  // All regular routes use the Angular engine
+  server.get('**', (req, res, next) => {
     const { protocol, originalUrl, baseUrl, headers } = req;
 
     commonEngine
@@ -45,10 +47,11 @@ export function app(): express.Express {
 function run(): void {
   const port = process.env['PORT'] || 4000;
 
-  //* Start up the Node server
+  // Start up the Node server
   const server = app();
   server.listen(port, () => {
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
 }
+
 run();
